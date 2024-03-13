4d3c106ae28e95059974f324a004183418322fca
LeonoffNSU <k.leonov@g.nsu.ru>
2024-03-13 20:13:36 +0700

    begin

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
 
+
+train_base = pd.read_csv('credit_train.csv')
+print(train_base)
